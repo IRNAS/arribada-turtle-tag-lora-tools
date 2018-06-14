75c7abf5ba389350789a2db96d219fdd48f1edab
Joshua Booth <joshua@icoteq.com>
2018-06-15 00:41:24 +0100

    backend: fix bug where USB would hang on packets of a multiple of 64

--- arribada_tools/backend.py
+++ arribada_tools/backend.py
@@ -2,7 +2,7 @@ import pyusb
 import message
 from array import array
 from ble import BluetoothTracker
-
+import time
 
 class ExceptionBackendNotFound(Exception):
     pass
@@ -89,7 +89,7 @@ class BackendUsb(_Backend):
 
         size = 0
         while data:
-            resp = self._usb.write(pyusb.EP_MSG_OUT, data[:512], timeout)
+            resp = self._usb.write(pyusb.EP_MSG_OUT, data[:63], timeout) # See https://www.microchip.com/forums/m818567.aspx for why a 1 less than 64 is sent
             resp.wait()
             if resp.status <= 0:
                 break
